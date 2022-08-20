42e1b33bf2a495daa06e8e0c4cf8d0765ad02714
tiresoti <dinarashhh@yandex.ru>
2022-08-21 00:36:16 +0300

    fix error

--- bubble_cpp.cpp
+++ bubble_cpp.cpp
@@ -5,14 +5,14 @@ int numbers[10] = {45678, 952, 1411, 44449, 0, 1, 33, 115, 228, 8};
 
 int main() {
 	int temp = 0;
-	for(int i = 0; i < sizeof(numbers)/sizeof(int); ++i)
+	for(int i = 0; i < sizeof(numbers)/sizeof(int) - 1; ++i)
 	{
-		for(int j = 0; j < sizeof(numbers)/sizeof(int)-1; ++j)
+		for(int j = 0; j < sizeof(numbers)/sizeof(int) - 1; ++j)
 		{
-			if(numbers[j] > numbers[j+1])
+			if(numbers[j] > numbers[j + 1])
 			{
 				temp = numbers[j];
-				numbers[j] = numbers[j+1];
+				numbers[j] = numbers[j + 1];
 				numbers[j+1] = temp;
 			}
 		}
